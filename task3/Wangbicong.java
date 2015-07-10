3143fea7a91891795db1758fb4f9715919f2dbae
Wangbicong <wangbicong@hotmail.com>
2015-07-10 23:10:52 +0800

    Update Wangbicong.java

--- task3/Wangbicong.java
+++ task3/Wangbicong.java
@@ -2,17 +2,19 @@ package addvote;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Random;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Timer;
+
 
 public class AddVote {
 
 	public static void main(String[] args){
 		try {
-			for(int i=0;i<100;i++){
-				URL url = new URL("http://today.hit.edu.cn/commend/113738_1.htm");
+			for(int i=1;i<=100;i++){
+				URL url = new URL("http://today.hit.edu.cn/commend/114093_1.htm");
 		    	HttpURLConnection con = (HttpURLConnection)url.openConnection();
 			    Random ran = new Random();
 			
@@ -22,19 +24,18 @@ public class AddVote {
 				int ip4 = ran.nextInt(255);
 				String ipAddress = ip1+"."+ip2+"."+ip3+"."+ip4;
 				
-			//	con.setRequestMethod("HEAD");  
+				//con.setRequestMethod("HEAD");
 				con.setRequestProperty("X-Forwarded-For",ipAddress);
 				
-				/*Map<String,List<String>> headerMap = con.getHeaderFields();  
+				Map<String,List<String>> headerMap = con.getHeaderFields();  
 			    Iterator<String> iterator = headerMap.keySet().iterator();  
 			    while (iterator.hasNext()) {  
 			        String key = iterator.next();  
 			        List<String> values = headerMap.get(key);  
 			          
 			        System.out.println(key + ":" + values.toString());  
-			    }  */
+			    }  
 				
-			    System.out.println(con.getResponseMessage());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
