81f3e9a6ef6c4389052cd4d011cdde83ca8deb71
593579831 <593579831@qq.com>
2015-07-12 21:13:57 +0800

    lvhaoyu task3

---
+++ task3/lvhaoyu/shuapiao.php
@@ -0,0 +1,20 @@
+﻿<?php
+    set_time_limit(0);
+    $i=0;
+    $url='http://today.hit.edu.cn/commend/114154_1.htm';
+    do{
+      $a = rand(1, 255);
+      $b = rand(1, 255);
+      $c = rand(1, 50);
+      $d = rand(1, 255);
+      $ip = "$a.$b.$c.$d";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-FORWARDED-FOR:'.$ip,'CLIENT-IP:'.$ip));
+      curl_setopt($ch, CURLOPT_HEADER,1);
+      curl_setopt($ch, CURLOPT_URL, $url);
+      sleep(2);
+      $i++;
+      $out = curl_exec($ch);
+      curl_close($ch);
+    }while($i<=100);
+?>
